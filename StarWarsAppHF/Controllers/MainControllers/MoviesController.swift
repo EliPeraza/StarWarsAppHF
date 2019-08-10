aacdf810254504f11179da8b8a673e57723b0541
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-08-10 14:11:34 -0400

    Storyboard set up: Tab bar, three main controllers -Movies, Characters, Planets-, three detailed controllers.

---
+++ StarWarsAppHF/Controllers/MainControllers/MoviesController.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesController.swift
+//  StarWarsAppHF
+//
+//  Created by Elizabeth Peraza  on 8/10/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class MoviesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
---
+++ StarWarsAppHF/Views/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  StarWarsAppHF
+//
+//  Created by Elizabeth Peraza  on 8/10/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+}
