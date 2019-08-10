71069f0164dfce19a66dbeaaf953515565c6220f
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-08-09 22:31:48 -0400

    Initial commit to test link between repo and local project. Test is a print statement on view controller

---
+++ StarWarsAppHF/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StarWarsAppHF
+//
+//  Created by Elizabeth Peraza  on 8/9/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    print("test to link retome repo")
+    
+  }
+
+
+}
+
