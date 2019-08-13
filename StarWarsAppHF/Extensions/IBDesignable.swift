ac8bde9bb90d67feaec7fb0839ad600bf65fddef
elizabethperaza@gmail.com <elizabethperaza@gmail.com>
2019-08-12 20:04:54 -0400

    added tab bar and app icons. Removed the IBDesignable file because I didn't use it

---
+++ StarWarsAppHF/Extensions/IBDesignable.swift
@@ -0,0 +1,9 @@
+//
+//  IBDesignable.swift
+//  StarWarsAppHF
+//
+//  Created by Elizabeth Peraza  on 8/12/19.
+//  Copyright © 2019 Elizabeth Peraza . All rights reserved.
+//
+
+import Foundation
